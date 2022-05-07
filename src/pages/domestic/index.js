577998611fa24aef91ab4df64298430d1badc7bf
jeremy.li <jeremy.li@basebit.ai>
2022-05-07 18:31:39 +0800

    国产肉类

--- src/pages/domestic/index.js
+++ src/pages/domestic/index.js
@@ -1,10 +1,48 @@
 import * as React from "react";
+import {StaticImage} from "gatsby-plugin-image";
 import Layout from "../../components/layout";
+import styled from "styled-components";
+import {Link} from "gatsby";
+
+const QuickJump = styled.div`
+  position: absolute;
+  top: 200px;
+  left: 50px;
+  right: 50px;
+  display: flex;
+  //justify-content: space-between;
+  //padding: 24px 262px;
+
+   a {
+    margin-right: 50px;
+    width: 600px;
+    height: 100px;
+    background-color: rgba(0, 0, 0, 0.8);
+    text-align: center;
+    line-height: 100px;
+    color: #fff;
+    font-size: 48px;
+    &,
+    :hover,
+    :active,
+    :visited {
+      text-decoration: none;
+    }
+    :hover {
+      background-color: #000;
+    }
+  }
+`;
 
 const DomesticPage = () => {
   return (
     <Layout>
-      <div>domestic</div>
+        <StaticImage src="../../images/domestic/01.jpg" style={{height: "100%"}}></StaticImage>
+        <QuickJump>
+            <Link to="/domestic">国产肉类</Link>
+            <Link to="/import">进口肉类</Link>
+        </QuickJump>
+
     </Layout>
   );
 };
